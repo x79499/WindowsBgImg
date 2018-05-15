a3ecd6cf5a430bd6fd0d0794f5e95df7365efa4a
赵粒博 <18582982968@qq.com>
2018-05-15 22:56:26 +0800

    添加程序唯一启动入口。

---
+++ BgImgSet/src/pers/zlb/start/Start.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package pers.zlb.start;
+
+import pers.zlb.util.BgImgSetFrame;
+
+/**
+ * @wsee.
+ * @see 小趙
+ * 项目名称：BgImgSet   
+ * 类 名  称：Start      
+ * 创 建  人：赵粒博   
+ * 创建时间：2018年5月15日 下午10:48:15   <br/>
+ * 类 描  述：   <br/>
+ *   
+ *     程序启动入口
+ *
+ */
+public class Start {
+	public static void main(String[] args) {
+		new BgImgSetFrame().StartUp();
+	}
+}
